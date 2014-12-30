17f4cef6fea8c8004cf3733df8f82321c6d589d8
Luke Madera <luke.madera@gmail.com>
2014-12-30 12:34:39 -0800

    test(items-list): 100% code coverage

--- app/src/modules/services/items-list/items-list.spec.js
+++ app/src/modules/services/items-list/items-list.spec.js
@@ -13,8 +13,27 @@ describe('appItemsList factory', function() {
 	// afterEach(function() {
 	// });
 	
-	/*
-	it('should do something', function() {
+	
+	it('should save items', function() {
+		var items =[
+			'one',
+			'two'
+		];
+		appItemsList.save(items, {});
+		expect(appItemsList.items[0]).toBe(items[0]);
 	});
-	*/
+
+	it('should read items', function() {
+		var items =appItemsList.read({});
+		expect(items[0]).toBeDefined();
+		expect(items[0].title).toBe('one');
+	});
+
+	it('should read items with existing items', function() {
+		appItemsList.items =['item1', 'item2'];
+		var items =appItemsList.read({});
+		expect(items[0]).toBeDefined();
+		expect(items[0]).toBe('item1');
+	});
+	
 });
